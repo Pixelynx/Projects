9d58b05b4e2d57daf9ecceaff26e3921a4347fc7
Pixelynx <tjstevens1992@gmail.com>
2019-02-09 18:47:57 -0500

    fixed construction

---
+++ joy_site/script.js
@@ -0,0 +1,30 @@
+const doc = document;
+
+doc.addEventListener("DOMContentLoaded", () => {
+
+  // let menuTab = doc.querySelector(".menu_tab");
+  //
+  // menuTab.addEventListener("mouseover", (event) => {
+  //   menuTab.style.transitionDelay = ".2s";
+  // })
+  //
+  // menuTab.addEventListener("mouseleave", (event) => {
+  //   // event.target.innerHTML = "";
+  // })
+
+  // let slideIdx = 0;
+  // const carousel = () => {
+  //   let slides = doc.querySelector(".slide");
+  //   for(let i = 0; i < slides.length; i++) {
+  //     slides[i].style.display = "none";
+  //   }
+  //   slideIdx++;
+  //   if(slideIdx > slides.length) { slideIdx = 1 }
+  //   slides[slideIdx-1].style.display = "block";
+  //   setTimeout(carousel, 2000);
+  // }
+  // carousel(slideIdx);
+
+
+
+})
